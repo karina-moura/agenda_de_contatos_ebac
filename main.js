c7e2e7159305c7772eae6903720374514faead8b
karina moura <kahmourasouza33@gmail.com>
2023-02-21 22:33:45 -0300

    Adicionando JavaScript

---
+++ main.js
@@ -0,0 +1,27 @@
+const form = document.getElementById("form-data");
+let linhas ='';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const inputNome = document.getElementById('nome')
+    const inputNumero = document.getElementById('numero')
+
+
+    let linha = '<tr>';
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputNumero.value}</td>`;;
+    linha += `</tr>`;
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas;
+
+    inputNome.value = "";
+    inputNumero.value = "";
+
+
+
+
+});
